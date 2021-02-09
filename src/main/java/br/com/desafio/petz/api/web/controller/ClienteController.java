e00644ab42f443b1e6a900e46522e6865ab53676
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-09 20:04:58 -0300

    Implementação dos Controlles

--- src/main/java/br/com/desafio/petz/api/web/controller/ClienteController.java
+++ src/main/java/br/com/desafio/petz/api/web/controller/ClienteController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.desafio.petz.api.dto.ClienteDto;
-import br.com.desafio.petz.api.dto.DataDto;
 import br.com.desafio.petz.api.model.Cliente;
 import br.com.desafio.petz.api.service.ClienteService;
 import br.com.desafio.petz.api.service.conveter.Converter;
@@ -42,139 +41,153 @@ import br.com.desafio.petz.api.web.response.ResponseApiPaged;
 @RestController
 @RequestMapping("/rest")
 public class ClienteController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private ClienteService service;
-	
+
 	@Autowired
 	private Converter<Cliente, ClienteDto> converter;
-	
-	@GetMapping(path="/clientes",produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(path = "/clientes", produces = MediaType.APPLICATION_JSON_VALUE)
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Response<ClienteDto>> listarClientes(){ 
+	public ResponseEntity<Response<ClienteDto>> listarClientes() {
 		LOGGER.info("LISTAR_CLIENTES");
 		Response<ClienteDto> response;
-		
-		List<Cliente>  clientes = null;
+
+		List<Cliente> clientes = null;
 		response = new ResponseApi<ClienteDto>();
-		try {
-			
+
 			clientes = service.findAll();
-			if(clientes.isEmpty())
-				return new ResponseEntity<Response<ClienteDto>>(response, HttpStatus.NO_CONTENT) ;		
-			
-			List<ClienteDto> dtos = converter.convertListToListDto(clientes); 
-			
+			if (clientes.isEmpty())
+				return new ResponseEntity<Response<ClienteDto>>(response, HttpStatus.NO_CONTENT);
+
+			List<ClienteDto> dtos = converter.convertListToListDto(clientes);
+
 			response.setData(dtos);
-		} catch (Exception e) {
-			LOGGER.error(ConstanteUtil.INTERNAL_SERVER_ERROR + e.getMessage() + "ERRO_LISTAR_CLIENTE");
-			throw new InternalServerException(ConstanteUtil.ERRO+ e.getMessage() + "ERRO_LISTAR_CLIENTE",e);
-		}
 		
-		
-		return new ResponseEntity<Response<ClienteDto>>(response, HttpStatus.OK) ;
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-	
-	
-	@GetMapping(path="/clientes/pagedAndSorted",produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> listarClientesPaged(@PageableDefault(size = 3) Pageable page){ 
+
+	@GetMapping(path = "/clientes/pagedAndSorted", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> listarClientesPaged(@PageableDefault(size = 3) Pageable page) {
 		LOGGER.info(" LISTAR_CLIENTES_PAGED ");
 		ResponseApiPaged<Page<Cliente>> clienteResponse = new ResponseApiPaged<Page<Cliente>>();
-		Page<Cliente>  clientes = null;
+		Page<Cliente> clientes = null;
 		try {
 			clientes = service.findAll(page);
 			clienteResponse.setData(clientes);
+		} catch (BusinessException ex) {
+			LOGGER.error(ConstanteUtil.INTERNAL_SERVER_ERROR + ex.getMessage() + "LISTAR_CLIENTES_PAGED");
+			throw new BusinessException(ex.getMessage(), ex);
 		} catch (Exception e) {
 			LOGGER.error(ConstanteUtil.INTERNAL_SERVER_ERROR + e.getMessage() + "LISTAR_CLIENTES_PAGED");
-			throw new InternalServerException(ConstanteUtil.ERRO+ e.getMessage() + "ERRO_LISTAR_CLIENTE",e);
+			throw new InternalServerException(ConstanteUtil.ERRO + e.getMessage() + "ERRO_LISTAR_CLIENTE", e);
 		}
-		
-		if(!clienteResponse.getDataPaged().hasContent()) 
-			return new ResponseEntity<>(clienteResponse, HttpStatus.NO_CONTENT) ;		
-		
-		return new ResponseEntity<>(clienteResponse, HttpStatus.OK) ;
+
+		if (!clienteResponse.getDataPaged().hasContent())
+			return new ResponseEntity<>(clienteResponse, HttpStatus.NO_CONTENT);
+
+		return new ResponseEntity<>(clienteResponse, HttpStatus.OK);
 	}
 	
-	@PostMapping(path="/clientes",produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> criarCliente(@RequestBody @Valid ClienteDto dto){
-		LOGGER.info("Criando CLIENTE "+ dto.getNome());
-		Cliente cliente = null;
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<Object> getClienteById(@PathVariable Long id) {
+		LOGGER.info("BUSCAR CLIENTE " + id);
+		Optional<Cliente> cliente;
 		ResponseApi<ClienteDto> clienteResponse = new ResponseApi<ClienteDto>();
-		try {
-			
-			cliente = converter.converteDtoToEntity(dto);			
-			cliente = service.salvar(cliente);
-		
-		
-			List<ClienteDto> listDataTtoResponse = new ArrayList<ClienteDto>();
-			listDataTtoResponse.add(converter.convertToDto(cliente));
-			clienteResponse.setData(listDataTtoResponse);
-		} catch (ResourceNotFoundException ex) {
-			throw ex;
-		} catch(Exception ex){
-			LOGGER.error(ex.getMessage() +" Cliente: "+ dto.getNome());
-			throw new InternalServerException(ex.getMessage(),ex);
+
+		cliente = service.buscarPorId(id);
+		if (cliente.isPresent()) {
+			List<ClienteDto> asList = Arrays.asList(converter.convertToDto(cliente.get()));
+			clienteResponse.setData(asList);
 		}
-		return new ResponseEntity<>(clienteResponse, HttpStatus.CREATED) ;
+		
+		return new ResponseEntity<>(clienteResponse, HttpStatus.OK);
+		
+	}	
+	
+	@GetMapping("/clientes/nome/{nome}")
+	public ResponseEntity<Object> getClienteByNome(@PathVariable String nome) {
+		LOGGER.info("BUSCAR CLIENTE POR NOME " + nome);
+		Optional<List<Cliente>> clientes;
+		ResponseApi<ClienteDto> clienteResponse = new ResponseApi<ClienteDto>();
+
+		clientes = service.buscarPorNome(nome);
+		if (clientes.isPresent()) {
+			List<ClienteDto> asList = Arrays.asList(converter.convertToDto(clientes.get().get(0)));
+			clienteResponse.setData(asList);
+		}
+		
+		return new ResponseEntity<>(clienteResponse, HttpStatus.OK);
+		
+	}	
+	
+	@PostMapping(path = "/clientes", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> criarCliente(@RequestBody @Valid ClienteDto dto) {
+		LOGGER.info("Criando CLIENTE " + dto.getNome());
+
+		Optional<Cliente> clienteOpt = null;
+		List<ClienteDto> listDataTtoResponse = new ArrayList<ClienteDto>();
+		ResponseApi<ClienteDto> clienteResponse = new ResponseApi<ClienteDto>();
+
+		clienteOpt = Optional.of(service.salvar(converter.converteDtoToEntity(dto, new Cliente()).get()));
+
+		listDataTtoResponse.add(converter.convertToDto(clienteOpt.get()));
+		clienteResponse.setData(listDataTtoResponse);
+		
+
+		return new ResponseEntity<>(clienteResponse, HttpStatus.CREATED);
 	}
 
-
 	/**
-	 *   --Alterar um Cliente---
-	 *  Esse método da API deve alterar alguma propriedade do Cliente.
+	 * --Alterar um Cliente--- 
+	 * Esse método pode alterar alguma propriedade do
+	 * Cliente.
 	 * 
-	 * @param IdCliente
-	 * {@code Request:	{ "payment_date" : "2018-06-30" }	 }		
+	 * @param IdCliente {@code }
 	 * 
 	 * @param id
 	 * @return 204 No Content.
 	 */
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<Object> alterarCliente(@RequestBody @Valid DataDto dataPagamento, @PathVariable Long id) {
-		LOGGER.info("UPDATE CLIENTE " + id );
-		
-		
-		Optional<Cliente> clienteOptional = service.verificarSeClienteExiste(id);
-        if(!clienteOptional.isPresent()) {
-			throw new ResourceNotFoundException("Erro ao realizar update no Cliente "+id);
-		}
-
-		try {
-			service.salvar(clienteOptional.get());
-		} catch (Exception e) {
-			LOGGER.error(e.getLocalizedMessage(), e.toString());
-			throw new InternalServerException(e.getMessage(), e);
-		}
-		ResponseApi<Cliente> ClienteResponse = new ResponseApi<Cliente>();
-		ClienteResponse.setData(Arrays.asList(clienteOptional.get()));
-		return ResponseEntity.noContent().build();
-	}
 	
-	/**
-	 *  
-	 * @param id idCliente
-	 * @return Cliente
-	 */
+	public ResponseEntity<Object> alterarCliente(@RequestBody ClienteDto dto, @PathVariable Long id) {
+		LOGGER.info("UPDATE CLIENTE " + id);
+		Optional<Cliente> cliente;
+		cliente = service.buscarPorId(id);
+		if (cliente.isPresent()) {
+			cliente = converter.converteDtoToEntity(dto, cliente.get());
+			service.alterar(cliente.get());
+		}
+		
+		return ResponseEntity.noContent().build();
+		
+	}		
+
 	@DeleteMapping("/clientes/{id}")
 	public ResponseEntity<Object> excluirCliente(@PathVariable Long id) {
-		LOGGER.info("Cancelar pedido: " + id );
-		
-			
-		Optional<Cliente> clienteOpt = service.verificarSeClienteExiste(id);
-    	if(!clienteOpt.isPresent()) {
-    		throw new ResourceNotFoundException("Error ao realizar delete do Cliente " + id);
-		}
+		LOGGER.info("Excluir cliente: {}", id);
 
-		try {			
-			service.excluir(clienteOpt.get().getId());	
-		} catch (Exception e) {
-			LOGGER.error("INTERNAL_SERVER_ERROR ", e.toString());
-			throw new InternalServerException(" ERRO_CANCELAR_CLIENTE " + id,e);
-		}	
+		Optional<Cliente> cliente = service.buscarPorId(id);
+		if (cliente.isPresent()) {
+			try {
+				service.excluir(id);
+			} catch (ResourceNotFoundException ex) {
+				LOGGER.error(ex.getMessage(), ex.toString());
+				throw new ResourceNotFoundException(" ERRO AO EXCLUIR CLIENTE " + id);
+			} catch (BusinessException ex) {
+				LOGGER.error(ex.getMessage(), ex.toString());
+				throw new BusinessException(" ERRO AO EXCLUIR CLIENTE" + id);
+			} catch (Exception e) {
+				LOGGER.error(String.format("INTERNAL_SERVER_ERROR : %s", e.toString()));
+				throw new InternalServerException(" ERRO AO EXCLUIR CLIENTE " + id, e);
+			}
+		}
+		
 		return ResponseEntity.noContent().build();
 	}
-	
-	
+
 }
