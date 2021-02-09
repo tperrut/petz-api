d24096f7de0424ebc7f00dae07e3522b2a4e4638
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-09 19:44:46 -0300

    Remoção do metodo toString().

--- src/main/java/br/com/desafio/petz/api/model/enuns/EnumTipo.java
+++ src/main/java/br/com/desafio/petz/api/model/enuns/EnumTipo.java
@@ -2,19 +2,14 @@ package br.com.desafio.petz.api.model.enuns;
 
 public enum EnumTipo {
 	AVE(1), CACHORRO(2), GATO(3), PEIXE(4), OUTROS(5);
-	
+
 	private int id;
 
 	private EnumTipo(int id) {
 		this.id = id;
 	}
-	
+
 	public int id() {
 		return this.id;
-	}
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
 	}
 }
