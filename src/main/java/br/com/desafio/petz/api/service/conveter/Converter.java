04df5183cc9093f1b28b9aee0c1c0c45cfd9a170
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-09 19:53:18 -0300

    Refatorando a saída dos métodos
    - Adicinando retorno como classe Optional
    - Criar de novo metodo para ser usado ao criar um novo Pet

--- src/main/java/br/com/desafio/petz/api/service/conveter/Converter.java
+++ src/main/java/br/com/desafio/petz/api/service/conveter/Converter.java
@@ -1,13 +1,18 @@
 package br.com.desafio.petz.api.service.conveter;
 
 import java.util.List;
+import java.util.Optional;
+
+import br.com.desafio.petz.api.model.Cliente;
 
 
 public interface Converter<T, E> {
 	
 		List<E> convertListToListDto(List<T> lista);
 		E convertToDto(T entry);
-		T converteDtoToEntity(E dto);
+		Optional<T> converteDtoToEntity(E dto, T cliente);
+		Optional<T> converteDtoToEntity(E dto);
+
 		List<T> convertListDtoToListEntity(List<E> dtos);
 	
 }
