e15ff82abadf87bb72133c30f2406b324aac3421
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-18 17:20:14 -0300

    add profile coverage

--- deployment/run-tests.sh
+++ deployment/run-tests.sh
@@ -1,14 +1,15 @@
 echo '========================================================' 
 echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ======================'
 echo '================= Iniciando os Testes =================='
+echo '============ Verificando Regras de Coverage ========'
 
 
-
-
-mvn test -P test 
-
+# Rodando os testes
+# mvn clean test -P test -Djacoco.skip=true
+# mvn clean verify sonar:sonar -Pcoverage -Dsonar.projectKey=tperrut_petz-api
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Pcoverage -Dsonar.projectKey=tperrut_petz-api
 
 
 echo '========================================================'
 echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ======================'
-echo '=================== Fim dos Testes ====================='
+echo '======================== Fim ========================'
