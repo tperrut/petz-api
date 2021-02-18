88600058da2ec31e307c0ef0df156bd16ccf237f
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-18 17:05:52 -0300

    correção na propriedade projectKey d sonar

--- deployment/run-tests.sh
+++ deployment/run-tests.sh
@@ -1,24 +1,19 @@
 echo '========================================================' 
 echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ======================'
 echo '================= Iniciando os Testes =================='
+echo '============ Verificando Regras de Coverage ========'
 
 
 # Rodando os testes
 # mvn clean test -P test -Djacoco.skip=true
-mvn clean verify sonar:sonar -Pcoverage -Dsonar.projectKeytperrut_petz-api
-echo '========================================================'
-echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ====================='
-echo '=================== Fim dos Testes ====================='
+mvn clean verify sonar:sonar -Pcoverage -Dsonar.projectKey=tperrut_petz-api
 
 
-echo '========================================================' 
-echo '========================================================'
-echo '============ Verificando Regras de Coverage ========'
 # rules configuradas no POM
 # mvn clean verify -P coverage
 
 
-
+echo '========================================================'
 echo '========================================================'
 echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ======================'
-echo '=================== Fim Coverage ====================='
+echo '=================== Fim  ====================='
