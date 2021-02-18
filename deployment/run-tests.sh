a01672ee9aaa6a83f2a9420f29515318fa89a76b
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-18 18:19:59 -0300

    teste sem verify

--- deployment/run-tests.sh
+++ deployment/run-tests.sh
@@ -6,14 +6,15 @@ echo '============ Verificando Regras de Coverage ========'
 
 # Rodando os testes
 # mvn clean test -P test -Djacoco.skip=true
-mvn clean verify install sonar:sonar \
--Pcoverage \
--Dsonar.projectKey=tperrut_petz-api \
--Dsonar.organization=tperrut \
--Dsonar.host.url=https://sonarcloud.io \
+# mvn clean verify install sonar:sonar \
+ mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar  
+#-Pcoverage \
+#-Dsonar.projectKey=tperrut_petz-api \
+#-Dsonar.organization=tperrut \
+#-Dsonar.host.url=https://sonarcloud.io \
 
 
-# mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar  \
+
 
 
 
