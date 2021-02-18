fdb98e13f9d3dfd17b7edcf9e14d05622c826eee
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-18 17:11:05 -0300

    add script gerado pelo sonarCloud

--- deployment/run-tests.sh
+++ deployment/run-tests.sh
@@ -6,7 +6,8 @@ echo '============ Verificando Regras de Coverage ========'
 
 # Rodando os testes
 # mvn clean test -P test -Djacoco.skip=true
-mvn clean verify sonar:sonar -Pcoverage -Dsonar.projectKey=tperrut_petz-api
+# mvn clean verify sonar:sonar -Pcoverage -Dsonar.projectKey=tperrut_petz-api
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=tperrut_petz-api
 
 
 # rules configuradas no POM
@@ -16,4 +17,4 @@ mvn clean verify sonar:sonar -Pcoverage -Dsonar.projectKey=tperrut_petz-api
 echo '========================================================'
 echo '========================================================'
 echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ======================'
-echo '=================== Fim  ====================='
+echo '======================== Fim  =========================='
