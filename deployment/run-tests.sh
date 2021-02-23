13039a1cc01b1d5ef62db83c8e74a1fc0e8c618a
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-22 22:15:22 -0300

    dando permissão full p mvw + melhorias na organização e chamada dos scripts

--- deployment/run-tests.sh
+++ deployment/run-tests.sh
@@ -2,10 +2,12 @@ echo '========================================================'
 echo '====================  ¯\_༼ᴼل͜ᴼ༽_/¯‍  ======================'
 echo '================= Iniciando os Testes =================='
 echo '============ Verificando Regras de Coverage ========'
+echo '============ Integração com SonarCloud ========'
 
 
 # Rodando os testes
-mvn -P test clean verify sonar:sonar -Dsonar.login=$SONAR_API_KEY 
+mvn -P test clean  sonar:sonar -Dsonar.login=$SONAR_API_KEY 
+
 
 
 
