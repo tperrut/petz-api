f26904c72f81a0edf19e1751e08741f9a1d8b8e9
Thiago Perrut <41409309+tperrut@users.noreply.github.com>
2021-02-23 09:53:48 -0300

    fazendo build antes do testes

--- deployment/run-tests.sh
+++ deployment/run-tests.sh
@@ -6,7 +6,7 @@ echo '============ Integração com SonarCloud ========'
 
 
 # Rodando os testes
-mvn -P test clean  sonar:sonar -Dsonar.login=$SONAR_API_KEY -Dsonar.java.binaries=target/classes
+mvn -Ptest sonar:sonar -Dsonar.login=$SONAR_API_KEY -Dsonar.java.binaries=target/classes 
 
 
 
