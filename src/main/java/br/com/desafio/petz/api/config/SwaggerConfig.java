f1b5fc67d0056dc168e50f6829d4e04e6bce357d
Perrutinho <thi.perrut@gmail.com>
2021-04-26 16:00:14 -0300

    ajuste(by Sonar) na classe de config do swagger

--- src/main/java/br/com/desafio/petz/api/config/SwaggerConfig.java
+++ src/main/java/br/com/desafio/petz/api/config/SwaggerConfig.java
@@ -1,19 +1,15 @@
 package br.com.desafio.petz.api.config;
 
-import java.util.*;
-
+import br.com.desafio.petz.api.security.JwtUser;
+import br.com.desafio.petz.api.security.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import br.com.desafio.petz.api.security.JwtUser;
-import br.com.desafio.petz.api.security.util.JwtTokenUtil;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,6 +19,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -51,18 +49,18 @@ public class SwaggerConfig {
 		GrantedAuthority grantAdmin = new SimpleGrantedAuthority("ADMIN");
 		GrantedAuthority grantUser= new SimpleGrantedAuthority("USUARIO");
 
-		UserDetails userDetailsAdmin = (UserDetails) new JwtUser(1L,"admin@petzapi.com", "admin12345", Arrays.asList(grantAdmin));
-		UserDetails userDetails = (UserDetails) new JwtUser(2L,"user@petzapi.com", "user12345", Arrays.asList(grantUser));
+		UserDetails userDetailsAdmin =  new JwtUser(1L,"admin@petzapi.com", "admin12345", Arrays.asList(grantAdmin));
+		UserDetails userDetails = new JwtUser(2L,"user@petzapi.com", "user12345", Arrays.asList(grantUser));
 		
 		return new InMemoryUserDetailsManager(Arrays.asList(userDetailsAdmin,userDetails));
 	}
 
 	@Bean
-	public SecurityConfiguration security() {
+	public SecurityConfiguration security(JwtTokenUtil tokenUtil, UserDetailsService userDetailsService ) {
 		String token;
 		try {
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@petzapi.com");
-			token = this.jwtTokenUtil.obterToken(userDetails);
+			UserDetails userDetails = userDetailsService.loadUserByUsername("admin@petzapi.com");
+			token = jwtTokenUtil.obterToken(userDetails);
 		} catch (Exception e) {
 			token = "";
 		}
